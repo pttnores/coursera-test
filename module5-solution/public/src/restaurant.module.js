4d63d73ceec26e5472528ea6658e224dc881aa46
José Nores <pttnores@gmail.com>
2017-08-01 13:58:40 -0300

    Module 5

--- module5-solution/public/src/restaurant.module.js
+++ module5-solution/public/src/restaurant.module.js
@@ -4,7 +4,7 @@
 /**
  * Restaurant module that includes the public module as a dependency
  */
-angular.module('restaurant', ['public', 'ngMock'])
+angular.module('restaurant', ['public'])
 .config(config);
 
 config.$inject = ['$urlRouterProvider'];
